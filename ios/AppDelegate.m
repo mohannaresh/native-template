55587dd796630644bf988ae74e02e509c38445d6
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2020-01-08 13:08:34 +0100

    Implementation details for firebase on iOS

--- ios/AppDelegate.m
+++ ios/AppDelegate.m
@@ -1,9 +1,17 @@
+#import <Firebase.h>
 #import "AppDelegate.h"
 #import "MendixNative/MendixNative.h"
+#import "RNFirebase/RNFirebaseNotifications.h"
+#import "RNFirebase/RNFirebaseMessaging.h"
 
 @implementation AppDelegate
 
 - (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  if ([ReactNative.instance useFirebase]) {
+    [FIRApp configure];
+    [RNFirebaseNotifications configure];
+  }
+
   NSBundle *mainBundle = [NSBundle mainBundle];
   NSString *targetName = [mainBundle objectForInfoDictionaryKey:@"TargetName"] ?: @"";
 
@@ -21,6 +29,25 @@
   [ReactNative.instance start:[[MendixApp alloc] init:nil bundleUrl:bundleUrl runtimeUrl:runtimeUrl warningsFilter:none enableGestures:false clearDataAtLaunch:false reactLoadingStoryboard:nil]];
   
   return YES;
+}
+
+- (void) application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+  if ([ReactNative.instance useFirebase]) {
+    [[RNFirebaseNotifications instance] didReceiveLocalNotification:notification];
+  }
+}
+
+- (void) application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo
+fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
+  if ([ReactNative.instance useFirebase]) {
+    [[RNFirebaseNotifications instance] didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+  }
+}
+
+- (void) application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+  if ([ReactNative.instance useFirebase]) {
+    [[RNFirebaseMessaging instance] didRegisterUserNotificationSettings:notificationSettings];
+  }
 }
 
 @end
